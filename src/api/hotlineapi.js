cb393e6bb45b9b0224f2295e5d24bd5fd6e4fe65
unknown <thangnguyenquoc6797@gmail.com>
2019-05-13 15:01:31 +0700

    Do function edit and add hotlines

--- src/api/hotlineapi.js
+++ src/api/hotlineapi.js
@@ -5,3 +5,23 @@ export async function getHotlines(){
     const url = `${HEROKU_URL}/hotlines`;
     return await axios.get(url);
 }
+
+export async function getHotlinesbyID(id){
+    const url = `${HEROKU_URL}/hotlines/${id}`;
+    return await axios.get(url);
+}
+
+export async function addHotline(hotline){
+    const url = `${HEROKU_URL}/hotlines`;
+    return await axios.post(url, hotline);
+}
+
+export async function editHotline(id, hotline){
+    const url = `${HEROKU_URL}/hotlines/${id}`;
+    return await axios.put(url, hotline);
+}
+
+export async function deleteHotline(id){
+    const url = `${HEROKU_URL}/hotlines/${id}`;
+    return await axios.delete(url);
+}
--- src/components/common/body/ManageHotline.js
+++ src/components/common/body/ManageHotline.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Sidebar from '../sidebar/Sidebar';
-import { getHotlines } from '../../../api/hotlineapi';
+import { getHotlines, getHotlinesbyID, editHotline, addHotline, deleteHotline } from '../../../api/hotlineapi';
+import { Modal, Button, InputGroup, FormControl } from 'react-bootstrap';
 
 class ManageHotline extends Component {
 
@@ -8,16 +9,25 @@ class ManageHotline extends Component {
     super(props);
     this.state = {
       hotlines: [],
-      isShowHotline: true
+      isShowHotline: true,
+      shouldShowEdit: false,
+      selectedID: '',
+      selectedArea: '',
+      selectedHotline: '',
+      hotline_area: '',
+      hotline_number: ''
     }
     this.handShowInputHotline = this.handShowInputHotline.bind(this);
+    this.handleShowEdit = this.handleShowEdit.bind(this);
+    this.handleCloseEdit = this.handleCloseEdit.bind(this);
+    this.handleEdit = this.handleEdit.bind(this);
+    this.handleAddHotline = this.handleAddHotline.bind(this);
   }
 
   componentDidMount() {
     if (this.state.hotlines.length === 0) {
       getHotlines().then(res => {
         this.setState({ hotlines: res.data })
-        console.log(res.data)
       });
     }
   }
@@ -26,6 +36,83 @@ class ManageHotline extends Component {
     this.setState({
       isShowHotline: !this.state.isShowHotline
     })
+  }
+
+  /* Modal show edit */
+
+  handleShowEdit = (id) => {
+    getHotlinesbyID(id).then(res => {
+      this.setState({
+        selectedID: id,
+        selectedArea: res.data.area,
+        selectedHotline: res.data.hotline_number,
+        shouldShowEdit: true
+      })
+    }
+    );
+  }
+
+  handleCloseEdit() {
+    this.setState({
+      shouldShowEdit: false
+    })
+  }
+
+  handleChangInputEditArea = event => {
+    this.setState({
+      selectedArea: event.target.value
+    })
+  }
+
+  handleChangInputEditHotline = event => {
+    this.setState({
+      selectedHotline: event.target.value
+    })
+  }
+
+  handleEdit() {
+    const hotline = {};
+    if (this.state.selectedArea.length > 0 && this.state.selectedHotline > 0) {
+      hotline['area'] = this.state.selectedArea
+      hotline['hotline_number'] = this.state.selectedHotline
+      editHotline(this.state.selectedID, hotline).then(res => {
+        getHotlines().then(res => {
+          this.setState({ hotlines: res.data })
+        })
+      })
+    }
+    this.handleCloseEdit();
+  }
+
+  handleChangInputAddArea = event => {
+    this.setState({
+      hotline_area: event.target.value
+    })
+  }
+
+  handleChangInputAddHotline = event => {
+    this.setState({
+      hotline_number: event.target.value
+    })
+  }
+
+  handleAddHotline() {
+    const hotline = {};
+    if (this.state.hotline_area.length > 0 && this.state.hotline_number.length > 0) {
+      hotline['area'] = this.state.hotline_area
+      hotline['hotline_number'] = this.state.hotline_number
+      addHotline(hotline).then(res => {
+        getHotlines().then(res => {
+          this.setState({ hotlines: res.data })
+        });
+        this.setState({
+          hotline_area: '',
+          hotline_number: ''
+        })
+      })
+    } else {
+      alert("You have to text in input")
+    }
   }
 
   render() {
@@ -55,10 +142,10 @@ class ManageHotline extends Component {
                     {
                       !this.state.isShowHotline &&
                       <div className="input-group mb-3">
-                        <input onChange={this.handleChangInputAddArea} type="text" className="form-control" placeholder="Area" aria-label="Recipient's username" aria-describedby="basic-addon2" />
-                        <input onChange={this.handleChangInputAddHotline} type="text" className="form-control" placeholder="Hotline number" aria-label="Recipient's username" aria-describedby="basic-addon2" />
+                        <input value={this.state.hotline_area} onChange={this.handleChangInputAddArea} type="text" className="form-control" placeholder="Area" aria-label="Recipient's username" aria-describedby="basic-addon2" />
+                        <input value={this.state.hotline_number} onChange={this.handleChangInputAddHotline} type="text" className="form-control" placeholder="Hotline number" aria-label="Recipient's username" aria-describedby="basic-addon2" />
                         <div className="input-group-append">
-                          <button onClick={this.handleAddCate} className="btn btn-outline-secondary">Submit</button>
+                          <button onClick={this.handleAddHotline} className="btn btn-outline-secondary">Submit</button>
                         </div>
                       </div>
                     }
@@ -78,10 +165,10 @@ class ManageHotline extends Component {
                               return <tbody key={index}>
                                 <tr>
                                   <td> {getHotlines.id} </td>
-                                  <td>{getHotlines.area}</td>
-                                  <td>{getHotlines.hotline_number}</td>
+                                  <td> {getHotlines.area} </td>
+                                  <td> {getHotlines.hotline_number} </td>
                                   <td>
-                                    <button className="ml-3 fa fa-edit"></button>
+                                    <button onClick={() => { this.handleShowEdit(getHotlines.id) }} className="ml-3 fa fa-edit"></button>
                                     <button className="ml-3 fa fa-trash"></button>
                                   </td>
                                 </tr>
@@ -97,6 +184,40 @@ class ManageHotline extends Component {
             </div>
           </div>
         </div>
+
+        {/* Modal show edit */}
+        <Modal show={this.state.shouldShowEdit} onHide={this.handleCloseEdit}>
+          <Modal.Header closeButton>
+            <Modal.Title>Edit This Hotline</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <InputGroup className="mb-3">
+              <InputGroup.Prepend>
+                <InputGroup.Text id="inputGroup-sizing-default">{this.state.selectedID}</InputGroup.Text>
+              </InputGroup.Prepend>
+              <FormControl
+                aria-label="Default"
+                aria-describedby="inputGroup-sizing-default"
+                value={this.state.selectedArea}
+                onChange={this.handleChangInputEditArea}
+              />
+              <FormControl
+                aria-label="Default"
+                aria-describedby="inputGroup-sizing-default"
+                value={this.state.selectedHotline}
+                onChange={this.handleChangInputEditHotline}
+              />
+            </InputGroup>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={this.handleCloseEdit}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={this.handleEdit}>
+              Edit
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </div>
     );
   }
